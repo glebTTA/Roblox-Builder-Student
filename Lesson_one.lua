c83bf82a6096d6b8798febbf5c228a847ec0143a
glebTTA <gleb@techtrainasia.com>
2024-12-20 22:41:39 +0700

    Update Lesson_one.lua

--- Lesson_one.lua
+++ Lesson_one.lua
@@ -1,3 +1,20 @@
+-- Score script ---
+
+print("The Game is Starting...")
+
+-- Now, we will create a variable to keep track of our score
+
+_G.score = 0
+
+-- This code will help us add 1 point to our score each second
+
+while true do
+	wait(1) -- wait 1 sesond before adding a point
+	_G.score = _G.score + 1 -- add 1 point to our score
+	print(score)
+end
+
+-- Reset script ---
 -- this script will reset the Global points variable on player death.
 game:GetService('Players').PlayerAdded:Connect(function(player)
 	player.CharacterAdded:Connect(function(character)
