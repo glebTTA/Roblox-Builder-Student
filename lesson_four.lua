bacd95f3bf9e61b375005ed706aa4fe7a600d79a
glebTTA <gleb@techtrainasia.com>
2024-12-23 00:01:47 +0700

    Create lesson_four.lua

---
+++ lesson_four.lua
@@ -0,0 +1,9 @@
+-- close button script
+script.Parent.MouseButton1Click:Connect(function()
+ script.Parent.Parent.Visible = False
+end)
+
+-- shop button script
+script.Parent.MouseButton1Click:Connect(function()
+ script.Parent.ShopFrame.Visible = true
+end)
